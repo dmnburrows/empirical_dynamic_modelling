fb00594a01a5a69a9b5587d310a0d1bc3355773b
Dominic Burrows <dominicburrows@hotmail.co.uk>
2025-10-23 13:57:13 -0700

    comparing E and PR

--- workspace.py
+++ workspace.py
@@ -1,45 +1,70 @@
-import time
+#Import packages
+#---------------------------------------
+import sys
 import os
-import subprocess
+import glob
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib
 import kedm
+import warnings
+import gc
+from multiprocessing import Pool
+from tqdm import tqdm
 
-star = time.time()
-print('running kedm')
+#Import your modules
+#---------------------------------------
+import CCM as cfn
+import proc_functions as pfn
 
-env = os.environ.copy()
-env['HDF5_DISABLE_VERSION_CHECK'] = '1'
+sys.path.insert(0, os.path.expanduser('~/Documents/admin_tools'))
+from admin_tools import admin_functions as adfn
 
-# Run the command
-cmd = [
-    "edm-xmap",
-    "--dataset", "data",
-    "--rho",
-    "--rho-diff",
-    "/snl/scratch25/dburrows/CCM/prc.h5",
-    "/snl/scratch25/dburrows/CCM/prac_xmap.h5"
-]
+# Define paths
+#----------------------------------------------------------------------
+code_path = '~/Documents/empirical_dynamic_modelling'
+data_path = '/snl/scratch25/dburrows/CCM'
+mask_l = np.sort(glob.glob(f'{data_path}/*mask*'))
 
-# Run it and wait for it to finish
-subprocess.run(cmd, env=env, check=True)
-
-end = time.time()
-print(f'Time kedm = {end-star}')
+sys.version
 
 
+# confirm -> on sparse data - e.g. subsample
+#=========================================
+#participation ratio - use PCA
 
 
-star = time.time()
-print('running pykedm')
+def compute_pr(data):
 
-import h5py
+    dic = cfn.xtr(data)
+    X = dic['trace'].T
+    scale = StandardScaler().fit_transform(X)
+    pca = PCA().fit(scale)
+    eigvals = pca.explained_variance_
+    d_pr = (eigvals.sum()**2) / np.sum(eigvals**2)
 
-# Add it to the HDF5 input file
-with h5py.File('/snl/scratch25/dburrows/CCM/prc.h5', 'a') as f:
-    print(list(f.keys()))
-    d=f['data'][:]
-    em=f['e'][:]
+    if '_sz_' in data or '_pre_' in data:cond = data.split('_')[-2]
+    else:cond = data.split('_')[-3]
+    name = dic['name']
+    
+    return(name, cond, d_pr)
 
-kedm.xmap(d, edims=em, tau=1, Tp=0)  
-  
-end = time.time()
-print(f'Time pykedm = {end-star}')
+
+    
+    
+if __name__ == "__main__":
+    from sklearn.decomposition import PCA
+    from sklearn.preprocessing import StandardScaler
+    n_proc = 8
+    print(f"Running on {n_proc} cores...")
+
+    with Pool(processes=n_proc) as pool:
+        results = list(tqdm(pool.imap_unordered(compute_pr, mask_l), total=len(mask_l)))
+
+    # unpack results into dataframe
+    name_l, cond_l, data_l = zip(*results)
+    fin = pd.DataFrame({'pr': data_l, 'name': name_l, 'cond': cond_l})
+    fin.to_csv(f"{data_path}/participation_ratio_PCA.csv", index=False)
+    print("Saved:", f"{data_path}/participation_ratio_PCA.csv")
+    
